039033f5746aee6f51eb3603fdae3013d34808aa
shram78 <ra00275@gmail.com>
2021-01-03 16:23:27 +0300

    for check

---
+++ ex2_2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*Написать программу, которая будет выполняться до тех пор, пока не будет введено слово exit.*/
+namespace ex2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Вводи любой текс, пока не надоест. Для выхода из игры напечатай слово exit\n");
+            string forExit = "exit";
+            string myInput = "";
+            while (forExit!= myInput)
+            {
+                Console.Write("Вводи слово... ");
+                myInput = Console.ReadLine();
+            }
+            Console.WriteLine("Валим!");
+            Console.ReadLine();
+        }
+    }
+}
